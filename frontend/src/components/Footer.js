8e727b7e4d52507dbb6ab8e6d39517621a786ae9
Anis-khemiri <aniskhemiri6@gmail.com>
2023-07-04 17:24:42 +0100

    add1234

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -6,7 +6,7 @@ export default function Footer() {
     <MDBFooter  className='myFooter container-fluid text-center text-lg-start text-muted'>
       <section className='d-flex justify-content-center justify-content-lg-between p-4 border-bottom'>
         <div className='me-5 d-none d-lg-block'>
-          <span>Get connected with us on social networks:</span>
+          <span>Get connected with us on social networks :</span>
         </div>
 
         <div className='iconFooter'>
